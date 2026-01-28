0eeef28d0d46e84d71c3137f522400c280eb4e4e
Henrik Persson <henrik.persson@onelegstudios.com>
2026-01-28 20:11:07 +0100

    Add dev login link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,28 +9,13 @@
             @csrf
 
             <!-- Email Address -->
-            <flux:input
-                name="email"
-                :label="__('Email address')"
-                :value="old('email')"
-                type="email"
-                required
-                autofocus
-                autocomplete="email"
-                placeholder="email@example.com"
-            />
+            <flux:input name="email" :label="__('Email address')" :value="old('email')" type="email" required
+                autofocus autocomplete="email" placeholder="email@example.com" />
 
             <!-- Password -->
             <div class="relative">
-                <flux:input
-                    name="password"
-                    :label="__('Password')"
-                    type="password"
-                    required
-                    autocomplete="current-password"
-                    :placeholder="__('Password')"
-                    viewable
-                />
+                <flux:input name="password" :label="__('Password')" type="password" required
+                    autocomplete="current-password" :placeholder="__('Password')" viewable />
 
                 @if (Route::has('password.request'))
                     <flux:link class="absolute top-0 text-sm end-0" :href="route('password.request')" wire:navigate>
@@ -55,5 +40,11 @@
                 <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
             </div>
         @endif
+
+        @env('local')
+            <div class="flex items-center justify-center gap-4 p-4 rounded-lg border text-zinc-600 dark:text-zinc-400">
+                <x-login-link email="test@example.com" :userAttributes="['name' => 'Test user']" label="Login as test user" />
+            </div>
+        @endenv
     </div>
 </x-layouts::auth>
