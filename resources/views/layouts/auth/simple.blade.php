fffeb106d2c7be4615b2245b599a25c4a25ab376
Henrik Persson <henrik.persson@onelegstudios.com>
2026-02-06 00:28:05 +0100

    ordered tailwind classes

--- resources/views/layouts/auth/simple.blade.php
+++ resources/views/layouts/auth/simple.blade.php
@@ -1,10 +1,10 @@
 <x-layouts::base :title="$title ?? null">
 
-    <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-        <div class="flex w-full max-w-sm flex-col gap-2">
+    <div class="flex flex-col items-center justify-center gap-6 p-6 bg-background min-h-svh md:p-10">
+        <div class="flex flex-col w-full max-w-sm gap-2">
             <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
-                <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
-                    <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                <span class="flex items-center justify-center mb-1 rounded-md h-9 w-9">
+                    <x-app-logo-icon class="text-black fill-current size-9 dark:text-white" />
                 </span>
                 <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
             </a>
--- resources/views/layouts/auth/split.blade.php
+++ resources/views/layouts/auth/split.blade.php
@@ -1,13 +1,13 @@
 <x-layouts::base :title="$title ?? null">
 
     <div
-        class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+        class="relative grid flex-col items-center justify-center px-8 h-dvh sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div
-            class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
+            class="relative flex-col hidden h-full p-10 text-white bg-muted lg:flex dark:border-e dark:border-neutral-800">
             <div class="absolute inset-0 bg-neutral-900"></div>
             <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
-                <span class="flex h-10 w-10 items-center justify-center rounded-md">
-                    <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
+                <span class="flex items-center justify-center w-10 h-10 rounded-md">
+                    <x-app-logo-icon class="text-white fill-current me-2 h-7" />
                 </span>
                 {{ config('app.name', 'Laravel') }}
             </a>
@@ -29,8 +29,8 @@
             <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                 <a href="{{ route('home') }}" class="z-20 flex flex-col items-center gap-2 font-medium lg:hidden"
                     wire:navigate>
-                    <span class="flex h-9 w-9 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                    <span class="flex items-center justify-center rounded-md h-9 w-9">
+                        <x-app-logo-icon class="text-black fill-current size-9 dark:text-white" />
                     </span>
 
                     <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
