c2d1e9db71e72c348c751e53b5c5f2fd6540a4fa
Henrik Persson <henrik.persson@onelegstudios.com>
2026-02-25 14:50:02 +0100

    Move local login link to the correct position in the login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,12 @@
         <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
 
+        @env('local')
+            <div class="flex items-center justify-center p-4 border rounded-lg gap-4 text-zinc-600 dark:text-zinc-400">
+                <x-login-link email="test@example.com" :userAttributes="['name' => 'Test user']" label="Login as test user" />
+            </div>
+        @endenv
+
         <form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-6">
             @csrf
 
@@ -41,10 +47,6 @@
             </div>
         @endif
 
-        @env('local')
-            <div class="flex items-center justify-center p-4 border rounded-lg gap-4 text-zinc-600 dark:text-zinc-400">
-                <x-login-link email="test@example.com" :userAttributes="['name' => 'Test user']" label="Login as test user" />
-            </div>
-        @endenv
+
     </div>
 </x-layouts::auth>
